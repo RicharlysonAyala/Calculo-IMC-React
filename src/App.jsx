1bf202673c71ed905a357d97a6ac6fdb5bdb9df6
richarlysonayala <richarlisonayala@gmail.com>
2024-05-05 12:24:34 -0300

    Base principal do projeto

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { data } from "./data/data";
+
+import { useState } from "react";
+
+import "./App.css";
+
+import ImcCalc from "./components/ImcCalc";
+import ImcTable from "./components/ImcTable";
+
+function App() {
+    const calcImc = (e, height, weight) => {
+      e.preventDefault();
+
+      const heightFloat = +height.replace(",", ".");
+      const weightFloat = +weight.replace(",", ".");
+
+      if(!heightFloat || !weightFloat) return;
+
+      const imcResult = (weightFloat / (heightFloat * heightFloat)).toFixed(1);
+
+      setImc(imcResult);
+
+      data.forEach((item) => {
+        if (imcResult >= item.min && imcResult <= item.max) {
+          setInfo(item.info);
+          setInfoClass(item.infoClass);
+        }
+      });
+
+      if (!info) return;
+    };
+
+    const resetBack = (e) => {
+      e.preventDefault();
+      setImc("");
+      setInfo("");
+      setInfoClass("");
+    }
+
+    const [imc, setImc] = useState("");
+    const [info, setInfo] = useState("");
+    const [infoClass, setInfoClass] = useState("");
+
+    return <div className="container">{!imc ? (<ImcCalc calcImc={calcImc} />) : (<ImcTable data={data} imc={imc} info={info} infoClass={infoClass} resetBack={resetBack} />)}</div>;
+};
+
+export default App;
